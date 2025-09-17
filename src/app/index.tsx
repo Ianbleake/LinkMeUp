9b1c283ca5ddc2395c9c26a5db71e38c3f452dbd
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2025-09-17 13:21:51 -0600

    feat: add shadc UI lib

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 function App() {
 
   return (
     <div className="text-lg font-semibold text-emerald-600">
       hola mundo
+      <Button>Click me</Button>
     </div>
   )
 }
