5ee3c2cdba8fd63ea0cc9f206123137581774b2c
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2025-09-19 13:52:05 -0600

    feat: add table styles

--- src/Pages/Files/ContactsTable/index.tsx
+++ src/Pages/Files/ContactsTable/index.tsx
@@ -16,33 +16,91 @@ export const ContactsTable = ({
   contacts,
 }: ContactsTableProps ):React.ReactElement => {
   return (
-    <div className="px-4" >
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>No.</TableHead>
-            <TableHead>Nombre</TableHead>
-            <TableHead>Apellido</TableHead>
-            <TableHead>Email</TableHead>
-            <TableHead>Pais</TableHead>
-            <TableHead>Teléfono</TableHead>
-            <TableHead>Empresa</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {contacts.map((contact, index) => (
-            <TableRow key={index}>
-              <TableCell>{index + 1}</TableCell>
-              <TableCell>{contact.Nombre}</TableCell>
-              <TableCell>{contact.Apellido}</TableCell>
-              <TableCell>{contact.Correo}</TableCell>
-              <TableCell>{contact.Pais}</TableCell>
-              <TableCell>{contact.Numero || "-"}</TableCell>
-              <TableCell>{contact.Empresa || "-"}</TableCell>
+    <div className="px-14 mt-4">
+      <div className="rounded-md border-2 border-gray-200 shadow-sm overflow-hidden">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className='border-r-2 border-gray-200 shadow-sm'>
+                <p className='text-center text-md text-gray-900'>
+                  No.
+                </p>
+              </TableHead>
+              <TableHead className='border-r-2 border-gray-200 shadow-sm'>
+                <p className='text-center text-md text-gray-900'>
+                  Nombre
+                </p>
+              </TableHead>
+              <TableHead className='border-r-2 border-gray-200 shadow-sm'>
+                <p className='text-center text-md text-gray-900'>
+                  Apellido
+                </p>
+              </TableHead>
+              <TableHead className='border-r-2 border-gray-200 shadow-sm'>
+                <p className='text-center text-md text-gray-900'>
+                  Email
+                </p>
+              </TableHead>
+              <TableHead className='border-r-2 border-gray-200 shadow-sm'>
+                <p className='text-center text-md text-gray-900'>
+                  Pais
+                </p>
+              </TableHead>
+              <TableHead className='border-r-2 border-gray-200 shadow-sm'>
+                <p className='text-center text-md text-gray-900'>
+                  Teléfono
+                </p>
+              </TableHead>
+              <TableHead >
+                <p className='text-center text-md text-gray-900'>
+                  Empresa
+                </p>
+              </TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHeader>
+          <TableBody>
+            {contacts.map((contact, index) => (
+              <TableRow key={index}>
+                <TableCell className='border-r-2 border-gray-200 shadow-sm'>
+                  <p className='text-center text-sm text-gray-500'>
+                    {index + 1}
+                  </p>
+                </TableCell>
+                <TableCell className='border-r-2 border-gray-200 shadow-sm'>
+                  <p className='text-center text-sm text-gray-500'>
+                    {contact.Nombre}
+                  </p>
+                </TableCell>
+                <TableCell className='border-r-2 border-gray-200 shadow-sm'>
+                  <p className='text-center text-sm text-gray-500'>
+                    {contact.Apellido}
+                  </p>
+                </TableCell>
+                <TableCell className='border-r-2 border-gray-200 shadow-sm'>
+                  <p className='text-center text-sm text-gray-500'>
+                    {contact.Correo}
+                  </p>
+                </TableCell>
+                <TableCell className='border-r-2 border-gray-200 shadow-sm'>
+                  <p className='text-center text-sm text-gray-500'>
+                    {contact.Pais}
+                  </p>
+                </TableCell>
+                <TableCell className='border-r-2 border-gray-200 shadow-sm'>
+                  <p className='text-center text-sm text-gray-500'>
+                    {contact.Numero || "-"}
+                  </p>
+                </TableCell>
+                <TableCell>
+                  <p className='text-center text-sm text-gray-500'>
+                    {contact.Empresa || "-"}
+                  </p>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   )
 }
