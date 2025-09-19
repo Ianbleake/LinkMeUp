d0a1df8a51d4b24ab6eaa2383ef849efe25b7a39
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2025-09-19 15:44:03 -0600

    feat: add version label

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,6 +5,7 @@ export const Footer = ():React.ReactElement => {
   return (
     <div className='bg-white min-h-16 flex flex-col items-center justify-center border-t border-gray-200 shadow'>
       <Logo className='py-5'/>
+      <span className='text-gray-400 text-sm'>V.1.0.0</span>
       <div className='mb-2'>
         <p className='text-gray-500 text-sm'>© 2025 <a href='https://github.com/Ianbleake' target='_blank' className='hover:text-emerald-600'>BleakeDev</a>. All rights reserved.</p>
       </div>
