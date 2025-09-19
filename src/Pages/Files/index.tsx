a8e3ae8b7429ad9c5f8e273a0f0656781d894946
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2025-09-19 17:33:27 -0600

    feat: add responsive on home page

--- src/Pages/Files/index.tsx
+++ src/Pages/Files/index.tsx
@@ -69,16 +69,14 @@ export const Files = (): React.ReactElement => {
 
   if(fileCount === 0) return <EmptyFiles />;
 
-  console.log(contacts);
-
   return (
     <div className="flex-1 flex flex-col p-8">
 
-      <div className="flex flex-row items-center justify-between pb-4 px-2 border-b border-gray-200">
+      <div className="flex flex-col items-center justify-between gap-5 pb-4 px-2 border-b border-gray-200 md:flex-row">
 
         <div className="flex flex-col gap-2">
-          <h2 className="text-2xl font-semibold text-emerald-600">Contactos importados</h2>
-          <p className="text-sm text-gray-500" >{ fileCount > 1 ? `Archivos (${fileCount}):` : 'Archivo:'} {fileNames}</p>
+          <h2 className="text-2xl font-semibold text-emerald-600 text-center  md:text-left">Contactos importados</h2>
+          <p className="text-sm text-gray-500 text-center md:text-left hidden md:block" >{ fileCount > 1 ? `Archivos (${fileCount}):` : 'Archivo:'} {fileNames}</p>
         </div>
       
         <ContactsExport contacts={contacts}/>
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -2,17 +2,19 @@ import React from 'react'
 import { MainInfo } from './MainInfo'
 import { Dropzone } from './Dropzone'
 
-export const Home = ():React.ReactElement => {
+export const Home = (): React.ReactElement => {
   return (
-    <div className='h-full flex flex-raw'>
+    <div className="h-full flex flex-col md:flex-row">
 
-      <div className='w-1/2 p-6'>
-        <MainInfo/>
+      <div className="w-full p-6 md:w-1/2">
+        <MainInfo />
       </div>
 
-      <div className='w-1/2 flex flex-col items-center gap-4 p-6'>
-        <h2 className='text-3xl font-semibold text-emerald-600 mt-4'>Carga aqui tu archivo:</h2>
-          <Dropzone/>
+      <div className="w-full flex flex-col items-center gap-4 p-6 md:w-1/2">
+        <h2 className="text-3xl font-semibold text-emerald-600 mt-4">
+          Carga aquí tu archivo:
+        </h2>
+        <Dropzone />
       </div>
 
     </div>
