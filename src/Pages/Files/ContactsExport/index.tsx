a922b0a5c0d7a769787eac0ed7822f60b8e4920a
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2025-09-19 15:41:41 -0600

    feat: add export contacts

--- src/Pages/Files/ContactsExport/index.tsx
+++ src/Pages/Files/ContactsExport/index.tsx
@@ -1,51 +1,54 @@
-import React from 'react'
+import React from "react";
 import { Button } from "@/components/ui/button";
 import { RiContactsBook2Fill } from "react-icons/ri";
 import { twTheme } from "@/Utils/ThemeColors";
 import { saveAs } from "file-saver";
+import countries from "i18n-iso-countries";
+import esLocale from "i18n-iso-countries/langs/es.json";
+import countryCodeMap from "./Utils/countryCodeMap";
+
+countries.registerLocale(esLocale);
 
 type ContactsExportProps = {
   contacts: Contact[];
-}
+};
 
-export const ContactsExport = ({
-  contacts,
-}:ContactsExportProps ):React.ReactElement => {
-  
+export const ContactsExport = ({ 
+  contacts 
+}: ContactsExportProps): React.ReactElement => {
+
   const handleExport = () => {
+    const vcfContent = contacts
+      .map((c) => {
+        const phone = c.Numero ? `+${c.Pais}${c.Numero}` : "";
 
-    const vcfContent = contacts.map(c => {
-      const phone = c.Numero ? `+${c.Pais}${c.Numero}` : "";
-    
-      return [
-        "BEGIN:VCARD",
-        "VERSION:3.0",
-        `N:${c.Apellido || ""};${c.Nombre || ""};;;`,
-        `FN:${c.Nombre || ""} ${c.Apellido || ""}`.trim(),
-        phone ? `TEL;TYPE=CELL:${phone}` : "",
-        c.Correo ? `EMAIL;TYPE=INTERNET:${c.Correo}` : "",
-        c.Empresa ? `ORG:${c.Empresa}` : "",
-        // Si quieres guardar el país textual en ADR
-        typeof c.Pais === "string" ? `ADR;TYPE=home:;;${c.Pais}` : "",
-        "END:VCARD"
-      ]
-        .filter(Boolean)
-        .join("\n");
-    }).join("\n");
-    
-    
+        const isoCode = countryCodeMap[Number(c.Pais)];
+        const countryName = isoCode ? countries.getName(isoCode, "es") : "";
+
+        return [
+          "BEGIN:VCARD",
+          "VERSION:3.0",
+          `N:${c.Apellido || ""};${c.Nombre || ""};;;`,
+          `FN:${c.Nombre || ""} ${c.Apellido || ""}`.trim(),
+          phone ? `TEL;TYPE=CELL:${phone}` : "",
+          c.Correo ? `EMAIL;TYPE=INTERNET:${c.Correo}` : "",
+          c.Empresa ? `ORG:${c.Empresa}` : "",
+          countryName ? `ADR;TYPE=home:;;${countryName}` : "",
+          "END:VCARD",
+        ]
+          .filter(Boolean)
+          .join("\n");
+      })
+      .join("\n");
 
     const blob = new Blob([vcfContent], { type: "text/vcard;charset=utf-8" });
     saveAs(blob, "contactos.vcf");
   };
 
   return (
-    <Button 
-      className="bg-emerald-600 cursor-pointer hover:bg-emerald-700"
-      onClick={handleExport}
-    >
+    <Button className="bg-emerald-600 cursor-pointer hover:bg-emerald-700" onClick={handleExport}>
       Importar contactos
       <RiContactsBook2Fill size={35} color={twTheme.colors.white} />
     </Button>
-  )
-}
+  );
+};
