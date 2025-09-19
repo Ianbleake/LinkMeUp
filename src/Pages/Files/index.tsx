651b1bbb7fed1b4322be4f992c2aeee2a3ed582a
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2025-09-19 13:02:01 -0600

    Feat: Add  download template

--- src/Pages/Files/index.tsx
+++ src/Pages/Files/index.tsx
@@ -102,32 +102,34 @@ export const Files = (): React.ReactElement => {
 
       </div>
 
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>No.</TableHead>
-            <TableHead>Nombre</TableHead>
-            <TableHead>Apellido</TableHead>
-            <TableHead>Email</TableHead>
-            <TableHead>Pais</TableHead>
-            <TableHead>Teléfono</TableHead>
-            <TableHead>Empresa</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {contacts.map((contact, index) => (
-            <TableRow key={index}>
-              <TableCell>{index + 1}</TableCell>
-              <TableCell>{contact.Nombre}</TableCell>
-              <TableCell>{contact.Apellido}</TableCell>
-              <TableCell>{contact.Correo}</TableCell>
-              <TableCell>{contact.Pais}</TableCell>
-              <TableCell>{contact.Numero || "-"}</TableCell>
-              <TableCell>{contact.Empresa || "-"}</TableCell>
+      <div className="px-4" >
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>No.</TableHead>
+              <TableHead>Nombre</TableHead>
+              <TableHead>Apellido</TableHead>
+              <TableHead>Email</TableHead>
+              <TableHead>Pais</TableHead>
+              <TableHead>Teléfono</TableHead>
+              <TableHead>Empresa</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHeader>
+          <TableBody>
+            {contacts.map((contact, index) => (
+              <TableRow key={index}>
+                <TableCell>{index + 1}</TableCell>
+                <TableCell>{contact.Nombre}</TableCell>
+                <TableCell>{contact.Apellido}</TableCell>
+                <TableCell>{contact.Correo}</TableCell>
+                <TableCell>{contact.Pais}</TableCell>
+                <TableCell>{contact.Numero || "-"}</TableCell>
+                <TableCell>{contact.Empresa || "-"}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
 
     </div>
   );
--- src/Pages/Home/MainInfo/DowloadTemplate/index.tsx
+++ src/Pages/Home/MainInfo/DowloadTemplate/index.tsx
@@ -2,10 +2,30 @@ import React from 'react'
 import { Button } from '@/components/ui/button'
 import { twTheme } from '@/Utils/ThemeColors';
 import { FaFileDownload } from "react-icons/fa";
+import * as XLSX from "xlsx";
+import { saveAs } from "file-saver";
 
 export const DowloadTemplate = (): React.ReactElement => {
+  const handleDownload = () => {
+    
+    const data = [
+      ["Nombre", "Apellido", "Pais", "Numero", "Correo", "Empresa",] //* encabezados
+    ];
+
+    const worksheet = XLSX.utils.aoa_to_sheet(data);
+    const workbook = XLSX.utils.book_new();
+
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Plantilla");
+
+    const wbout = XLSX.write(workbook, { bookType: "xlsx", type: "array" });
+    saveAs(new Blob([wbout], { type: "application/octet-stream" }), "LinkMeUp Template.xlsx");
+  };
+
   return (
-    <Button className='bg-emerald-600 cursor-pointer hover:bg-emerald-700 ' onClick={()=>console.log("Dowloading template...")}>
+    <Button 
+      className='bg-emerald-600 cursor-pointer hover:bg-emerald-700' 
+      onClick={handleDownload}
+    >
       Descargar plantilla
       <FaFileDownload size={30} color={twTheme.colors.white} />
     </Button>
